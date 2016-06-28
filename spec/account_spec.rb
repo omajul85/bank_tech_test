c38b15841e583facf1cbe698a7f80149a077da5e
Omar Alvarez <omajul2@hotmail.com>
2016-06-28 15:28:26 +0200

    Test Account creation with amount of money

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,11 +10,23 @@ describe Account do
 
 	describe "#initialization" do	
 
+		it "has an empty array of transactions" do
+			expect(account.transactions).to eq []
+		end
+		
 		context "when created with no money" do
 			it "has a balance of 0.0" do
 				expect(account.balance).to eq 0.0
 			end
 		end
+
+		context "when created with some amount of money" do
+			it "has a balance of that amount of money" do
+				dummy_account = Account.new 10
+				expect(dummy_account.balance).to eq 10.0
+			end
+		end
+
 	end
 
 end
